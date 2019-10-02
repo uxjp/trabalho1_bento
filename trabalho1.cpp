b323bafa6242556d6b323dee6a810dfcb536f572
Joao Pedro Goncalves Rocha Ribeiro <johnpnx@MacPro.local>
2019-10-02 15:59:18 -0300

    façam as contribuiçoes no develop deixando sempre o master como uma versao estavel2

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 double f_de_x(double a, double x)
 {
-    
     return a*exp(x) - 4*x*x;
 }
 
 double f_linha_de_x(double a, double x)
 {
-    
     return a*exp(x) - 8*x;
 }
 
 int main(int argc, char const *argv[])
 {   
     double amplitude = 0.5;
-
+/*/ 
+    calcular o isolamento 
+//*/
     for(int i = -10; i <=10; i = i + 1){
-        cout << "i  " << f_de_x(amplitude, i) << endl;    
+        cout << "i  " << f_de_x(amplitude, i) <<"  f'(x): "<< f_linha_de_x(amplitude, i) << endl;    
     }
     return 0;
 }
