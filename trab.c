392fa875b76fb386f303e072ecb9d8ff8a3b4b28
pedrosaints <48567256+pedrosaints@users.noreply.github.com>
2019-10-14 16:36:34 -0300

    ADICIONANDO A VALIDAÇÃO DO TAMANHO DE A
    Corrigindo um erro que era possível definir como tamanho 0 o tamanho do vetor de amplitudes

--- trab.c
+++ trab.c
@@ -37,7 +37,8 @@ int validarA(double a);
 int main(int argc, char const *argv[]) {	
 
   	double RT[3] = {0, 0, 0};
-  	int continuar = 1;
+  	int continuar1 = 1;
+    int continuar2 = 1;
     int indiceInserirA = 1;
 
 
@@ -62,60 +63,78 @@ int main(int argc, char const *argv[]) {
   	printf("INSIRA O ERRO MAXIMO\n");
   	scanf("%lf",&E);
 
-  	printf("\nINSIRA O NUMERO DE AMPLITUDES\n");
+
+
+    while(continuar1){
+
+    printf("\nINSIRA O NUMERO DE AMPLITUDES\n");
   	scanf("%d",&n);
 
-  	R = (double **) malloc(n * sizeof(double *));
 
-  //inicializando a matriz R
-  	for(j=1; j<=n; j++) {
-  	  R[j-1] = (double*) malloc(sizeof(double)*3);
-  	  for ( i=0; i<=2; i++) {
-  	    R[j-1][i]=0;
+    //validando n
+    if (n <=0) {
+
+      printf("INSIRA UM VALOR > 0 \n");
+
+    }
+
+    else { 
+
+      continuar1 = 0;
+
+  	  R = (double **) malloc(n * sizeof(double *));
+
+      //inicializando a matriz R
+  	  for(j=1; j<=n; j++) {
+  	    R[j-1] = (double*) malloc(sizeof(double)*3);
+  	    for ( i=0; i<=2; i++) {
+  	      R[j-1][i]=0;
+  	    }
   	  }
-  	}
 
 
-  	A = (double *) malloc(n * sizeof(double));
-	printf("INSIRA UM VALOR ENTRE [0 - INFINITO]\n");
-  	while(continuar){
-  	printf("\nINSIRA O %dº VALOR DE A: ",indiceInserirA);
-    scanf("%lf",&a_aux);
+  	  A = (double *) malloc(n * sizeof(double));
+	    printf("INSIRA UM VALOR ENTRE [0 - INFINITO]\n");
+  	  while(continuar2){
+  	  printf("\nINSIRA O %dº VALOR DE A: ",indiceInserirA);
+      scanf("%lf",&a_aux);
 
-    if (validarA(a_aux)){
-    	A[indiceInserirA-1] = a_aux;
-    	indiceInserirA++;
+      if (validarA(a_aux)){
+    	  A[indiceInserirA-1] = a_aux;
+    	  indiceInserirA++;
 
-    	if(indiceInserirA == n + 1){
-    		continuar = 0;
-    	}
-    }else{
-    	printf("INSIRA UM VALOR ENTRE [0 - INFINITO]\n");
+    	  if(indiceInserirA == n + 1){
+    		  continuar2 = 0;
+    	  }
+      }else{
+    	  printf("INSIRA UM VALOR ENTRE [0 - INFINITO]\n");
+      }  
+
     }  
-
-  }  
   //exibindo A
-  	printf("\nVETOR DE AMPLITUDES A:\n\n  A = [");
-  	for(j=1;j<=n-1;j++){
-  	  printf("%lf,",A[j-1]);
-  	}
-  	printf("%lf]\n\n",A[j-1]);
+  	  printf("\nVETOR DE AMPLITUDES A:\n\n  A = [");
+  	  for(j=1;j<=n-1;j++){
+  	    printf("%lf,",A[j-1]);
+  	  }
+  	  printf("%lf]\n\n",A[j-1]);
 
-  	for( j = 1; j <= n ; j++) {
-  	  printf("\n\n===== EXECUTANDO PARA a = %lf e E = %lf =====\n\n\n", A[j-1], E) ;
-  	  call_metodos( A[j-1], E, R[j-1]);
-  	}
+  	  for( j = 1; j <= n ; j++) {
+  	    printf("\n\n===== EXECUTANDO PARA a = %lf e E = %lf =====\n\n\n", A   [j-1], E) ;
+  	    call_metodos( A[j-1], E, R[j-1]);
+  	  }
 
-	printf("\n\n================ RESUMO ================\n");
-	printf("d1: Resultado Newton-Raphson\nd2: Resultado Newton-Raphson Modificado\nd3: Resultado Secante\n\n\n");
+	  printf("\n\n================ RESUMO ================\n");
+	  printf("d1: Resultado Newton-Raphson\nd2: Resultado Newton-Raphson        Modificado\nd3: Resultado Secante\n\n\n");
 
-  	for(j=1;j<=n;j++) {
-  	  printf("a = %lf | d1 = %0.20lf | d2 = %0.20lf | d3 = %0.20lf\n",A[j-1],R[j-1][0],R[j-1][1],R[j-1][2]);
-  	}
-  	free(A);
+  	  for(j=1;j<=n;j++) {
+  	    printf("a = %lf | d1 = %0.20lf | d2 = %0.20lf | d3 = %0.20lf\n",A     [j-1],R[j-1][0],R[j-1][1],R[j-1][2]);
+  	  }
+  	  free(A);
 
-	return 0;
+	  return 0;
 
+  }
+  }
 }
 
 int validarA(double a){
