ba6c555165a20eebb417d4be61e5693a2c7596e0
pedrosaints <48567256+pedrosaints@users.noreply.github.com>
2019-10-15 14:20:11 -0300

    Adicionando Erro Relativo e Numero de Iter.

--- metodos.h
+++ metodos.h
@@ -1,29 +1,31 @@
 // Metodo principal chama os demais metodos, armazena os resultados dos
 // respectivos métodos em uma estrutura de dados
-void call_metodos(double a_funcao, double E,double* R);
+void call_metodos(double a_funcao, double E,double* R,int* I,double* ER);
 
 double call_isolamento(double a_funcao, double *a_intervalo, double *b_intervalo);
 void isolamento(double a_funcao ,double* a_isolamento, double* b_isolamento);
 double resolver_f(double a, double d);
 
 
-double call_newton_raphson(double a_funcao, double raiz0, double E);
+double call_newton_raphson(double a_funcao, double raiz0, double E,int* K, double* ER);
 double newton_raphson(double x_barra, double f, double f_linha);
 //     resolver_f
 double resolver_f_linha(double a, double d);
 double erro(double x_barra_anterior, double x_barra_novo);
 
 
-double call_newton_raphson_modificado(double a_funcao, double raiz0, double E);
+double call_newton_raphson_modificado(double a_funcao, double raiz0, double E, int* K, double* ER);
 //     resolver_f
 //     resolver_f_linha
 //     newton_raphson
 //	   erro
 
 
-double call_metodo_secante(double a_funcao, double a_intervalo, double b_intervalo, double E);
+double call_metodo_secante(double a_funcao, double a_intervalo, double b_intervalo, double E, int* K, double* ER);
 //     erro
 //     resolver_f
 double numerador_secante(double a_funcao, double a_intervalo, double b_intervalo);
+
 double denominador_secante(double a_funcao, double a_intervalo, double b_intervalo);
+
 int validarA(double a);
