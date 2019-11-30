2eacb50390c5217a02fb98182acc7a26d215f2b8
Breno silva <breno.silva.2903@gmail.com>
2019-11-30 20:26:51 -0300

    Adição das interações

--- Trabalho 2/mn.h
+++ Trabalho 2/mn.h
@@ -18,7 +18,10 @@ void *vetor_diferenca_entre_dois_vetores(void *vetor_anterio, void *vetor_atual,
 double maior_valor_abs_vetor(void *vetor, int n);
 double erro_gauss_jacobi(void *vetor_anterio, void *vetor_diferenca, int n);
 void call_Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro);
-void *Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro);
+void *Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro, int *);
 void imprimir_matriz(void *matriz, int n);
 void imprimir_vetor(void *vetor, int n);
-bool criterio_de_linhas(void *matriz, int n);
+void *Gauss_Seidel(void *matriz_a, void *vetor_b, int n, double erro, int *);
+void *criar_vetor_x0_gauss_seidel(void *matriz, void *vetor, int n);
+void *criar_vetor_xk_gauss_seidel(void *matriz_a, void *vetor_b, void *vetor_xk, int n);
+void call_Gauss_Seidel(void *matriz_a, void *vetor_b, int n, double erro);
--- Trabalho 2/mn2.c
+++ Trabalho 2/mn2.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "mn.h"
@@ -19,7 +19,7 @@ int main(){
 
 void iniciar(void *matriz, void *vetor){
 	int n;
-	double erro;	
+	double erro;
 
 	printf("Informe o erro desejado: ");
 	scanf("%lf",&erro);
@@ -32,32 +32,21 @@ void iniciar(void *matriz, void *vetor){
 
 	if(matriz != NULL && vetor != NULL){
 		informar_valores_matriz_A(matriz, n);
-		informar_valores_vetor_B(vetor,n);
-
-		if (criterio_de_linhas(matriz, n)) call_Gauss_Jacobi(matriz, vetor, n, erro);
+		informar_valores_vetor_B(vetor,n);   
+   
+      	printf("\n\n\nGAUSS-JACOBI \n\n\n");
+    	call_Gauss_Jacobi(matriz, vetor, n, erro);	
+      	printf("\n\n\nGAUSS-SEIDEL \n\n\n");
+      	call_Gauss_Seidel(matriz, vetor, n, erro); 
 
 	}else{
 		printf("FALHA AO CRIAR MATRIZ OU AO CRIAR VETOR");
 	}	
 }
 
-bool criterio_de_linhas(void *matriz, int n) {
-	int l, c;
-	double pivo, somatLinha = 0;
-
-	for (l = 0; l < n; ++l) {
-		pivo = abs(matriz[l][l]);
-		
-		for (c = 0; c < n, ++c)  if (c != l) somatLinha += abs(matriz[l][c]);
-
-		if (pivo < somatLinha) return false;
-	}
-	
-	return true;
-}
-
 void call_Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro){
 	int linhas = 0, i, j, k;
+	int interacoes = 0, interacoes_total = 0;
 	double valor;
 	void *vetorResposta = NULL;
 	void *vetorValidar = NULL;
@@ -70,17 +59,22 @@ void call_Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro){
 	matrizIdentidade = criar_matriz_identidade(n);
 
 	for (i = 0; i < n; i++){
+		
 		for (j = 0; j < n; j++){			
 			valor = obter_matriz(matrizIdentidade, i, j, n);
 			atribuir_vetor(vetor_linha_identidade, j,valor);			
 		}
 		
-		vetor_linha_inversa = Gauss_Jacobi(matriz_a, vetor_linha_identidade, n, erro);
+    	printf("\nCALCULO COLUNA %d DA INVERSA:\n\n",i);
+		vetor_linha_inversa = Gauss_Jacobi(matriz_a, vetor_linha_identidade, n, erro, &interacoes);
 	
 		for(k = 0; k < n; k++){
 			valor = obter_vetor(vetor_linha_inversa, k);
 			atribuir_matriz(matrizInversa, k, i, valor, n);			
 		}
+
+		interacoes_total += interacoes;
+		interacoes = 0;
 	}
 
 
@@ -104,16 +98,21 @@ void call_Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro){
 	imprimir_vetor(vetorResposta, n);
 
 	printf("Vetor diferenca\n");
-	imprimir_vetor(vetorDif, n);	
+	imprimir_vetor(vetorDif, n);
+
+	printf("Total de interacoes: %d\n", interacoes_total);	
 	printf("****************************************\n");
 
 }
 
 void imprimir_vetor(void *vetor, int n){
 	int i;
-	for(i = 0; i < n; i++){
-		printf("%0.10lf   ", obter_vetor(vetor, i));
+	printf("[");
+	printf("%0.10lf", obter_vetor(vetor, 0));
+	for(i = 1; i < n; i++){
+		printf("   %0.10lf", obter_vetor(vetor, i));
 	}
+	printf("]");
 	printf("\n");
 }
 
@@ -127,23 +126,33 @@ void imprimir_matriz(void *matriz, int n){
 	}	
 }
 
-void *Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro){
+void *Gauss_Jacobi(void *matriz_a, void *vetor_b, int n, double erro, int *interacoes){
 		//Encontra os valores usando o método de Gauss Jacobe e retorna os valores em um vetor
-		int i;
+		int i = 0;		
 		void *vetor_x0;
 		void *vetor_xk;
 		void *vetor_diferenca;
 		
 		vetor_x0 = criar_vetor_x0_gauss_jacobi(matriz_a, vetor_b, n);		
+    	printf("vetor x%d: ",i);
+    	imprimir_vetor(vetor_x0,n);
+    	i++;
 		vetor_xk = criar_vetor_xk_gauss_jacobi(matriz_a, vetor_b, vetor_x0, n);
 		vetor_diferenca = vetor_diferenca_entre_dois_vetores(vetor_x0, vetor_xk, n);
+		(*interacoes) = 1;
 
-		while(erro_gauss_jacobi(vetor_xk, vetor_diferenca, n) > erro){		
-
+		while(erro_gauss_jacobi(vetor_xk, vetor_diferenca, n) > erro){
 			vetor_x0 = vetor_xk;
+      		printf("vetor x%d: ",i);
+      		imprimir_vetor(vetor_x0,n);
+      		i++;
 			vetor_xk = criar_vetor_xk_gauss_jacobi(matriz_a, vetor_b, vetor_x0, n);		
-			vetor_diferenca = vetor_diferenca_entre_dois_vetores(vetor_x0, vetor_xk, n);				
+			vetor_diferenca = vetor_diferenca_entre_dois_vetores(vetor_x0, vetor_xk, n);			
+			(*interacoes) += 1;				
 		}		
+
+  		printf("vetor x%d: ",i);
+  		imprimir_vetor(vetor_xk,n);
 
 	return vetor_xk;
 }
@@ -374,4 +383,172 @@ double erro_gauss_jacobi(void *vetor_anterio, void *vetor_diferenca, int n){
 
 void liberar_matriz(void *matriz){
     free(matriz);
+}
+
+void *Gauss_Seidel(void *matriz_a, void *vetor_b, int n, double erro, int *interacoes){
+		//Encontra os valores usando o método de Gauss Jacobe e retorna os valores em um vetor
+		int i = 0;
+		void *vetor_x0;
+		void *vetor_xk;
+		void *vetor_diferenca;
+		
+		vetor_x0 = criar_vetor_x0_gauss_seidel(matriz_a, vetor_b, n);		
+    printf("vetor x%d: ",i);
+    imprimir_vetor(vetor_x0,n);
+    i++;
+		vetor_xk = criar_vetor_xk_gauss_seidel(matriz_a, vetor_b, vetor_x0, n);
+		vetor_diferenca = vetor_diferenca_entre_dois_vetores(vetor_x0, vetor_xk, n);
+		(*interacoes) = 1;
+		while(erro_gauss_jacobi(vetor_xk, vetor_diferenca, n) > erro){		
+      
+			vetor_x0 = vetor_xk;
+      printf("vetor x%d: ",i);
+      imprimir_vetor(vetor_x0,n);
+      i++;
+			vetor_xk = criar_vetor_xk_gauss_seidel(matriz_a, vetor_b, vetor_x0, n);		
+			vetor_diferenca = vetor_diferenca_entre_dois_vetores(vetor_x0, vetor_xk, n);
+			(*interacoes) += 1;					
+		}		
+
+  printf("vetor x%d: ",i);
+  imprimir_vetor(vetor_xk,n);
+
+	return vetor_xk;
+}
+
+
+void *criar_vetor_x0_gauss_seidel(void *matriz, void *vetor, int n){
+	//Retorna o vetor X0 e tem como entrada a matriz A, vetor B e o tamanho(n)
+	int i;
+	void *vetor_x0 = criar_vetor(n);
+	double valor;
+	double valor_matriz;
+
+	if(vetor_x0 != NULL){
+
+		for(i = 0; i < n; i++){
+			valor_matriz = obter_matriz(matriz, i, i, n);
+			if(valor_matriz != 0){
+				valor = obter_vetor(vetor, i) / valor_matriz;
+				atribuir_vetor(vetor_x0, i, valor);
+			}else{
+				atribuir_vetor(vetor_x0, i, 0);	
+			}			
+		}
+		return vetor_x0;
+
+	}else{
+		printf("FALHA AO CRIAR O VETOR X0 NO GAUSS SEIDEL");
+		return NULL;
+	}
+}
+
+void *criar_vetor_xk_gauss_seidel(void *matriz_a, void *vetor_b, void *vetor_xk, int n){
+	/*
+	* Retorna o vetor com os novos valores
+	* vetor_xk = vetor com os valores anteriores
+	*/
+
+	void *vetor_xk_atual = criar_vetor(n);
+	int i, j;
+	double valor, pivo_matriz, valor_matriz, valor_vetor;
+
+	if(vetor_xk_atual != NULL){
+
+		for(i = 0; i < n; i++){
+
+			pivo_matriz = obter_matriz(matriz_a, i, i, n);
+			valor = obter_vetor(vetor_b, i);			
+
+			for(j = 0; j < n; j++){		
+
+				if(i != j){
+					valor_matriz = obter_matriz(matriz_a, i, j, n);		
+				}else{
+					valor_matriz = 0;
+				}							
+				
+        if (i<j) {
+				  valor -= (valor_matriz * obter_vetor(vetor_xk, j));
+        }
+        else {
+          valor -= (valor_matriz * obter_vetor(vetor_xk_atual, j));
+        }
+								
+			}	
+
+			if(pivo_matriz != 0){
+					valor = valor / pivo_matriz;
+				}else{
+					valor = 0;
+				}
+
+			atribuir_vetor(vetor_xk_atual, i, valor);
+		}
+
+		return vetor_xk_atual;
+	}else{
+		printf("FALHA AO CRIAR O VETOR XK NO GAUSS SEIDEL");
+		return NULL;
+	}
+}
+
+void call_Gauss_Seidel(void *matriz_a, void *vetor_b, int n, double erro){
+	int linhas = 0, i, j, k;
+	double valor;
+	int interacoes = 0, interacoes_total = 0;
+	void *vetorResposta = NULL;
+	void *vetorValidar = NULL;
+	void *vetorDif = NULL;
+	void *matrizIdentidade = NULL;	
+	void *vetor_linha_identidade = criar_vetor(n);
+	void *vetor_linha_inversa = criar_vetor(n);
+	void *matrizInversa = criar_matriz(n);
+	
+	matrizIdentidade = criar_matriz_identidade(n);
+
+	for (i = 0; i < n; i++){
+		for (j = 0; j < n; j++){			
+			valor = obter_matriz(matrizIdentidade, i, j, n);
+			atribuir_vetor(vetor_linha_identidade, j,valor);			
+		}
+		
+    printf("\nCALCULO COLUNA %d DA INVERSA:\n\n",i);
+		vetor_linha_inversa = Gauss_Seidel(matriz_a, vetor_linha_identidade, n, erro, &interacoes);
+	
+		for(k = 0; k < n; k++){
+			valor = obter_vetor(vetor_linha_inversa, k);
+			atribuir_matriz(matrizInversa, k, i, valor, n);			
+		}
+		interacoes_total += interacoes;
+		interacoes = 0;
+	}
+
+
+	vetorResposta = multiplicar_matriz_com_vetor(matrizInversa, vetor_b, n);
+
+	//Verificar a corretude dos valores encontrados
+	vetorValidar = multiplicar_matriz_com_vetor(matriz_a, vetorResposta, n);
+	vetorDif = vetor_diferenca_entre_dois_vetores(vetor_b, vetorValidar, n);
+
+	printf("****************************************\n");
+	printf("Matriz A\n");
+	imprimir_matriz(matriz_a, n);
+
+	printf("Vetor B\n");
+	imprimir_vetor(vetor_b, n);
+
+	printf("Matriz Inversa\n");
+	imprimir_matriz(matrizInversa, n);
+
+	printf("Vetor resposta\n");
+	imprimir_vetor(vetorResposta, n);
+
+	printf("Vetor diferenca\n");
+	imprimir_vetor(vetorDif, n);
+
+	printf("Total de interacoes: %d\n", interacoes_total);	
+	printf("****************************************\n");	
+	
+
 }
